9a455fb2ed37beeb1549f54f504581bb38c2f3f9
Juan Augusto Adad <juanadadbatero181@gmail.com>
2024-05-06 11:25:53 -0300

    añado carpeta de recursos para wiki

--- punto3.c
+++ punto3.c
@@ -2,28 +2,32 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define TAMA 5
-
 
 int main()
 {
-    char *nombres[TAMA];
+    char **nombres;
     char buff[10];
+    int cant;
 
-    for(int i=0; i<TAMA; i++)
+    printf("ingrese la cantidad de nombres: ");
+    scanf("%d",&cant);
+    nombres = (char**) malloc (sizeof(char*) * cant);
+
+    for(int i=0; i<cant; i++)
     {
         printf("ingrese el nombre %d: ", i+1);
+        fflush(stdin);
         gets(buff);
         nombres[i] = (char*) malloc (strlen(buff) + 1 * sizeof(char));
         strcpy(nombres[i], buff);
     }
     printf("\n");
-    for(int i=0; i<TAMA; i++)
+    for(int i=0; i<cant; i++)
     {
         puts(nombres[i]);
     }
 
-    for(int i=0; i<TAMA; i++)
+    for(int i=0; i<cant; i++)
     {
         free(nombres[i]);
     }
